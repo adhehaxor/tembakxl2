5daa69605dc581e32b9a7d93a1da008dac355be6
adhehaxor <39878379+adhehaxor@users.noreply.github.com>
2018-06-24 23:07:38 +0700

    Update __init__.py

--- xlpy/__init__.py
+++ xlpy/__init__.py
@@ -1,10 +1,10 @@
 from .config import Config
 from .client import XL
 
-__copyright__       = 'Copyright 2018 by Anggi Alberto'
-__version__         = '1.0.0'
-__author__          = 'Anggi Alberto'
-__author_email__    = 'albertoanggi@gmail.com'
-__url__             = 'http://github.com/albertoanggi/xl-py'
+__copyright__       = 'Copyright 2018 by Adhe Yusup'
+__version__         = '2.0.0'
+__author__          = 'Adhe Yusup'
+__author_email__    = 'adheyhaxor12@gmail.com'
+__url__             = 'https://github.com/adhehaxor/tembakxl2.git'
 
 __all__ = ['Config','XL']
